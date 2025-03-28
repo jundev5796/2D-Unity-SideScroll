751e6699f5de85bc05bf0ac80f0d500c1244813d
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-28 10:18:55 +0900

    Refactor code

--- Katana-SideScroll/Assets/Script/Player.cs
+++ Katana-SideScroll/Assets/Script/Player.cs
@@ -187,7 +187,7 @@ public class Player : MonoBehaviour
 
 
 
-
+    private const float GROUND_CHECK_DISTANCE = 0.7f;
 
 
 
@@ -195,57 +195,36 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Debug.DrawRay(pRig2D.position, Vector3.down, new Color(0, 0.7f, 0));
+        Debug.DrawRay(pRig2D.position, Vector3.down, new Color(0, GROUND_CHECK_DISTANCE, 0));
 
         //레이캐스트로 땅체크 
-        RaycastHit2D rayHit = Physics2D.Raycast(pRig2D.position, Vector3.down, 0.7f, LayerMask.GetMask("Ground"));
+        RaycastHit2D rayHit = Physics2D.Raycast(pRig2D.position, Vector3.down, GROUND_CHECK_DISTANCE, LayerMask.GetMask("Ground"));
 
-        if (pRig2D.linearVelocityY < 0)
+        CheckGroundedState(rayHit);
+    }
+
+
+    void CheckGroundedState(RaycastHit2D rayHit)
+    {
+        bool isGrounded = rayHit.collider != null && rayHit.distance < GROUND_CHECK_DISTANCE;
+
+        if (isGrounded)
         {
-            if (rayHit.collider != null)
-            {
-                if (rayHit.distance < 0.7f)
-                {
-                    pAnimator.SetBool("Jump", false);
-                }
-            }
-            else
-            {
-                //떨어지고 있다
-                if (!isWall)
-                {
-                    //그냥 떨어지는중
-                    pAnimator.SetBool("Jump", true);
-                }
-                else
-                {
-                    //벽타기
-                    pAnimator.SetBool("Grab", true);
-                }
-            }
+            pAnimator.SetBool("Jump", false);
         }
+
         else
         {
-            if (rayHit.collider != null)
+            //떨어지고 있다
+            if (!isWall)
             {
-                if (rayHit.distance < 0.7f)
-                {
-                    pAnimator.SetBool("Jump", false);
-                }
+                //그냥 떨어지는중
+                pAnimator.SetBool("Jump", true);
             }
             else
             {
-                //떨어지고 있다
-                if (!isWall)
-                {
-                    //그냥 떨어지는중
-                    pAnimator.SetBool("Jump", true);
-                }
-                else
-                {
-                    //벽타기
-                    pAnimator.SetBool("Grab", true);
-                }
+                //벽타기
+                pAnimator.SetBool("Grab", true);
             }
         }
     }
